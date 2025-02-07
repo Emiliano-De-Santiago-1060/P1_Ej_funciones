0325e8d99a47c0fe47705aace38e7f83b19b60c6
Emiliano-De-Santiago-1060 <164534381+Emiliano-De-Santiago-1060@users.noreply.github.com>
2025-02-07 16:47:39 +0000

    mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,18 @@
+//funcion que no devielva valor
+//funcion saludar
+
+void saludar(){
+  print("Hola mundo!");
+}
+
+void escribiendoNombre(){
+  print("Emiliano de Santiago Soto");
+}
+
+void main() {
+  //Emiliano de Santiago Soto: 22308051281060
+  //Llamar a la funcion saludar
+  print("Llamando a la funcion saludar");
+  saludar();
+  escribiendoNombre();
+}
