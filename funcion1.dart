6f834a9d4b71d43b8c009746bbff799321fd0e86
Emiliano-De-Santiago-1060 <164534381+Emiliano-De-Santiago-1060@users.noreply.github.com>
2025-02-10 18:04:24 +0000

    ejemplo funcion rama

--- funcion1.dart
+++ funcion1.dart
@@ -9,6 +9,30 @@ void escribiendoNombre(){
   print("Emiliano de Santiago Soto");
 }
 
+void sumar(double a, double b) {
+  double resultado = a + b;
+  print("La suma de $a y $b es: $resultado");
+}
+
+void restar(double a, double b) {
+  double resultado = a - b;
+  print("La resta de $a y $b es: $resultado");
+}
+
+void multiplicar(double a, double b) {
+  double resultado = a * b;
+  print("La multiplicación de $a y $b es: $resultado");
+}
+
+void dividir(double a, double b) {
+  if (b != 0) {
+    double resultado = a / b;
+    print("La división de $a entre $b es: $resultado");
+  } else {
+    print("No se puede dividir entre cero");
+  }
+}
+
 void main() {
   //Emiliano de Santiago Soto: 22308051281060
   //Llamar a la funcion saludar
@@ -16,4 +40,13 @@ void main() {
   print("Llamando a la funcion saludar");
   saludar();
   escribiendoNombre();
+
+  double num1 = 10;
+  double num2 = 5;
+
+  print("Calculadora Básica:");
+  sumar(num1, num2);
+  restar(num1, num2);
+  multiplicar(num1, num2);
+  dividir(num1, num2);
 }
---
+++ funcion2.dart
@@ -0,0 +1,16 @@
+void suma(int a, int b) {
+  int resultado = a + b;
+  print("La suma de $a y $b es: $resultado");
+}
+void resta(int a, int b) {
+  int resultado = a - b;
+  print("La resta de $a y $b es: $resultado");
+}
+// funcion main
+void main() {
+  print("Emiliano de Santiago Soto: 22308051281060");
+  int num1 = 10;
+  int num2 = 5;
+  suma(num1, num2);
+  resta(num1, num2);
+}
